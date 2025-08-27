16dbb03646a48d5f3932b6ef5edbe6761e35b161
MohamedBadawy <mohamed.m.badawii@gmail.com>
2025-08-27 16:32:29 +0300

    0.00002

---
+++ E-CommerceAppLoginPage/CartTabelViewCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CartTabelViewCellTableViewCell.swift
+//  E-CommerceAppLoginPage
+//
+//  Created by MohamedBadawi on 22/08/2025.
+//
+
+import UIKit
+
+class CartTabelViewCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
